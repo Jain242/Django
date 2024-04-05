411e688162b3f96955585b84d6d6e776b0c42ca2
Egor <agent-nino@bk.ru>
2024-04-05 20:48:29 +0300

    hw2

--- HW/HW_proj/apphw/models.py
+++ HW/HW_proj/apphw/models.py
@@ -1,3 +1,33 @@
+import datetime 
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    adreess = models.TimeField()
+    registration_date = models.DateField()
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+    add_date = models.DateTimeField(datetime.datetime.now())
+    
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    order_date = models.DateTimeField(datetime.datetime.now())
+
+    def __str__(self):
+        return f"Заказ #{self.id} от {self.client}"
--- HW/HW_proj/apphw/views.py
+++ HW/HW_proj/apphw/views.py
@@ -1,38 +1,78 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import logging
-
-logger = logging.getLogger('django')
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Client
+#from .forms import ClientForm 
 
 
-
-def index(request):
-    html = """
-    <html>
-    <head><title>Главная страница</title></head>
-    <body>
-        <h1>Добро пожаловать на мой первый Django-сайт!</h1>
-    </body>
-    </html>
-    """
-
-    log_data = f"VISIT MAIN PAGE IP: {request.META.get('REMOTE_ADDR')}, User-Agent: {request.META.get('HTTP_USER_AGENT')}"
-    logger.info(log_data) 
-    return HttpResponse(html)
+# logger = logging.getLogger('django')
 
 
-def about(request):
-    html = """
-    <html>
-    <head><title>О себе</title></head>
-    <body>
-        <h1>Обо мне</h1>
-        <p>Привет! Это мой сайт на Django.</p>
+
+# def index(request):
+#     html = """
+#     <html>
+#     <head><title>Главная страница</title></head>
+#     <body>
+#         <h1>Добро пожаловать на мой первый Django-сайт!</h1>
+#     </body>
+#     </html>
+#     """
+
+#     log_data = f"VISIT MAIN PAGE IP: {request.META.get('REMOTE_ADDR')}, User-Agent: {request.META.get('HTTP_USER_AGENT')}"
+#     logger.info(log_data) 
+#     return HttpResponse(html)
+
+
+# def about(request):
+#     html = """
+#     <html>
+#     <head><title>О себе</title></head>
+#     <body>
+#         <h1>Обо мне</h1>
+#         <p>Привет! Это мой сайт на Django.</p>
   
-    </body>
-    </html>
-    """
-    log_data = f"VISIT ABOUT PAGE. IP: {request.META.get('REMOTE_ADDR')}, User-Agent: {request.META.get('HTTP_USER_AGENT')}"
-    logger.info(log_data)  
+#     </body>
+#     </html>
+#     """
+#     log_data = f"VISIT ABOUT PAGE. IP: {request.META.get('REMOTE_ADDR')}, User-Agent: {request.META.get('HTTP_USER_AGENT')}"
+#     logger.info(log_data)  
 
-    return HttpResponse(html)
+#     return HttpResponse(html)
+
+
+
+
+def create_client(request):
+    if request.method == 'POST':
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('client_list')
+    else:
+        form = ClientForm()
+    return render(request, 'create_client.html', {'form': form})
+
+def client_list(request):
+    clients = Client.objects.all()
+    return render(request, 'client_list.html', {'clients': clients})
+
+def view_client(request, client_id):
+    client = get_object_or_404(Client, id=client_id)
+    return render(request, 'view_client.html', {'client': client})
+
+def update_client(request, client_id):
+    client = get_object_or_404(Client, id=client_id)
+    if request.method == 'POST':
+        form = ClientForm(request.POST, instance=client)
+        if form.is_valid():
+            form.save()
+            return redirect('client_list')
+    else:
+        form = ClientForm(instance=client)
+    return render(request, 'update_client.html', {'form': form, 'client': client})
+
+def delete_client(request, client_id):
+    client = get_object_or_404(Client, id=client_id)
+    if request.method == 'POST':
+        client.delete()
+        return redirect('client_list')
+    return render(request, 'delete_client.html', {'client': client})
